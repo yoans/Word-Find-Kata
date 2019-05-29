a3432d6030de72a3d5bfea5cdee6ae0dfb9749b8
Nathaniel Young <nathanyoans@gmail.com>
2019-05-29 18:03:29 -0500

    basic search diagonally ascending

--- test/find-word.spec.js
+++ test/find-word.spec.js
@@ -2,6 +2,7 @@ const findWord = require('../src/find-word');
 const {expect} = require('chai');
 const Chance = require('chance');
 const chance = new Chance();
+const {range} = require('ramda');
 describe('find words', ()=>{
     it('basic search horizontally', ()=>{
         const word = chance.word();
@@ -16,7 +17,7 @@ describe('find words', ()=>{
         const reversedWord = word.split('').reverse().join('');
         const grid = [reversedWord];
         const coordinates = findWord(grid)(word);
-        const expectedCoordinates = word.split('').map((_,index)=>`(${index},0)`).reverse().join(',')
+        const expectedCoordinates = word.split('').map((_,index)=>`(${index},0)`).reverse().join(',');
 
         expect(coordinates).to.eql(`${word}: ${expectedCoordinates}`);
     })
@@ -24,15 +25,23 @@ describe('find words', ()=>{
         const word = chance.word();
         const grid = word.split('');
         const coordinates = findWord(grid)(word);
-        const expectedCoordinates = word.split('').map((_,index)=>`(0,${index})`).join(',')
+        const expectedCoordinates = word.split('').map((_,index)=>`(0,${index})`).join(',');
 
         expect(coordinates).to.eql(`${word}: ${expectedCoordinates}`);
     })
     it('basic search vertically reversed', ()=>{
         const word = chance.word();
-        const grid = word.split('').reverse()
+        const grid = word.split('').reverse();
         const coordinates = findWord(grid)(word);
-        const expectedCoordinates = word.split('').map((_,index)=>`(0,${index})`).reverse().join(',')
+        const expectedCoordinates = word.split('').map((_,index)=>`(0,${index})`).reverse().join(',');
+
+        expect(coordinates).to.eql(`${word}: ${expectedCoordinates}`);
+    })
+    it('basic search diagonally ascending', ()=>{
+        const word = chance.word();
+        const grid = word.split('').reverse().map((character, yIndex)=>range(0, word.length).map((_,xIndex)=>((xIndex+yIndex)===word.length-1)?character:`.`).join(''));
+        const coordinates = findWord(grid)(word);
+        const expectedCoordinates = word.split('').map((_,index)=>`(${index},${word.length-1-index})`).join(',');
 
         expect(coordinates).to.eql(`${word}: ${expectedCoordinates}`);
     })
