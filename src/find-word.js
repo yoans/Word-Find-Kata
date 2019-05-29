31780361dfb51b42ab273ab69f0b12240f7f2add
Nathaniel Young <nathanyoans@gmail.com>
2019-05-29 17:02:44 -0500

    basic search vertically passes

--- src/find-word.js
+++ src/find-word.js
@@ -22,18 +22,43 @@ const getStartingCoordinateInfo = (grid, word) => {
     })
     return {startXIndex, startYIndex, reversed, foundWord}
 }
+const conditionallyReverse = (reversed, coordinates) => reversed ? coordinates.reverse() : coordinates;
+const convertToString = (word, orderedCoordinates) => `${word}: ${orderedCoordinates}`;
 const searchHorizontally = (grid, word) => {
     const {startXIndex, startYIndex, reversed, foundWord} = getStartingCoordinateInfo(grid, word);
 
     if(foundWord){
         const coordinates = range(0, word.length).map((index)=>`(${startXIndex+index},${startYIndex})`);
-        const orderedCoordinates = reversed ? coordinates.reverse() : coordinates;
-        return `${word}: ${orderedCoordinates}`
+        const orderedCoordinates = conditionallyReverse(reversed, coordinates);
+        return convertToString(word, orderedCoordinates);
+    };
+}
+const swapXandY = (grid) => {
+    const newGrid = [];
+    grid.forEach((row, yIndex)=>{
+        row.split('').forEach((character, xIndex)=>{
+            if(!newGrid[xIndex]){
+                newGrid[xIndex] = character
+            } else {
+                newGrid[xIndex] += character
+            }
+        });
+    });
+    return newGrid;
+};
+const searchVertically = (grid, word) => {
+    const gridTransform = swapXandY(grid);
+    const {startYIndex = startXIndex, startXIndex = startYIndex, reversed, foundWord} = getStartingCoordinateInfo(gridTransform, word);
+
+    if(foundWord){
+        const coordinates = range(0, word.length).map((index)=>`(${startXIndex},${startYIndex+index})`);
+        const orderedCoordinates = conditionallyReverse(reversed, coordinates);
+        return convertToString(word, orderedCoordinates);
     };
 }
 
 
 
 module.exports = (grid) => (word) => {
-    return searchHorizontally(grid, word);
+    return searchHorizontally(grid, word) || searchVertically(grid, word);
 }
