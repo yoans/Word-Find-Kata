a8f314ac79fce02c3d1c0aca90284cc245b21561
Nathaniel Young <nathanyoans@gmail.com>
2019-05-29 06:42:58 -0500

    improve test, search horizontally

--- test/find-word.spec.js
+++ test/find-word.spec.js
@@ -1,6 +1,13 @@
 const findWord = require('../src/find-word');
+const {expect} = require('chai');
+const Chance = require('chance');
+const chance = new Chance();
 describe('find words', ()=>{
-    it('search horizontally', ()=>{
+    it('basic search horizontally', ()=>{
+        const grid = [['SCOTTY']];
+        const word = chance.word();
         const coordinates = findWord(grid)(word);
+        
+        expect(coordinates).to.eql('SCOTTY: (0,0),(1,0),(2,0),(3,0),(4,0),(5,0)');
     })
 })
