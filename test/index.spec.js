9adbe737eaca4cd97b0f358b92b84d3f6d5f0de0
Nathaniel Young <nathanyoans@gmail.com>
2019-05-29 21:55:06 -0500

    oh deere it doesn't work

--- test/index.spec.js
+++ test/index.spec.js
@@ -27,4 +27,17 @@ describe('Run word find', ()=>{
         expect(fileParser.args).to.eql([[data]]);
         expect(findWords.args).to.eql([[parsedData]]);
     })
+    it.only('Live Test', ()=>{
+        const expectedLog = [
+            'BONES: (0,6),(0,7),(0,8),(0,9),(0,10)',
+            'KHAN: (5,9),(5,8),(5,7),(5,6)',
+            'KIRK: (4,7),(3,7),(2,7),(1,7)',
+            'SCOTTY: (0,5),(1,5),(2,5),(3,5),(4,5),(5,5)',
+            'SPOCK: (2,1),(3,2),(4,3),(5,4),(6,5)',
+            'SULU: (3,3),(2,2),(1,1),(0,0)',
+            'UHURA: (4,0),(3,1),(2,2),(1,3),(0,4)',
+        ].join('\n');
+        require(MODULE_PATH);
+        console.log({expectedLog});
+    })
 })
