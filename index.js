d32d70c5a7973748e1a4b3345abecf3d8ba75af8
Nathaniel Young <nathanyoans@gmail.com>
2019-05-30 21:37:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,22 @@ const findWords = require('./src/find-words');
 fs.readFile('./data/sample-grid.txt',(_,data)=>{
     console.log(findWords(fileParser(data)));
 });
+if(!fs.fake){
+  const Mocha = require('mocha')
+
+  const runner = new Mocha({})
+
+  runner.addFile('./test/file-parser.spec.js')
+  runner.addFile('./test/find-word.spec.js')
+  runner.addFile('./test/find-words.spec.js')
+  runner.addFile('./test/file-parser.spec.js')
+  runner.addFile('./test/index.spec.js')
+
+  runner.run(failures => {
+    if (failures) {
+      console.error(failures)
+    } else {
+      console.log('All passed.')
+    }
+  })
+}
